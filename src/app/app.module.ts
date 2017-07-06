75ba1c6ca1aa5ff2a0a5e6d15d899721d9219511
Byron Hambly <byron@clockboot.com>
2017-07-06 11:39:34 +0200

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { LocalStorageService } from 'ngx-webstorage';
 import { AppComponent } from './app.component';
 import { NewnameComponent } from './newname/newname.component';
 import { CounterComponent } from './counter/counter.component';
-import { LocalStorageService } from 'ngx-webstorage';
 import { RandomService } from './random.service';
 
 @NgModule({
